8f6984c3f04b12e2543cfb7e68d9e1bd2fcc89db
Calvin Donner <calvinjdonner@gmail.com>
2021-12-04 14:11:38 -0700

    set up User.js

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please provide a valid email address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+const User = model('User', UserSchema);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+module.exports = User;
