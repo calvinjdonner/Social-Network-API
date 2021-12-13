f779fd7e57207cc55bd9e774956803e393795723
Calvin Donner <calvinjdonner@gmail.com>
2021-12-13 14:00:40 -0700

    updated connection file

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  
+  // Use this to log mongo queries being executed!
+  module.exports = mongoose.connection; 
