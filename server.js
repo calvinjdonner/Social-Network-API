ac9980cde18e99f38c58f1c6c1c0c20a8093a55f
Calvin Donner <calvinjdonner@gmail.com>
2022-02-03 21:06:34 -0700

    updated connection and server files

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,16 +7,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
-
-// Use this to log mongo queries being executed!
-mongoose.set('debug', true);
-
-app.listen(PORT, () => console.log(`Connected to local host 3001`));
